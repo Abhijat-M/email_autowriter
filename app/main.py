0e9a41a4525eccf2c22ed95f13624619133a4487
abhijat-0 <abhijatknight003@gmail.com>
2025-04-19 13:12:36 +0530

    update

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from langchain_community.document_loaders import WebBaseLoader
 
-from chains import Chain
-from portfolio import Portfolio
-from utils import clean_text
+from app.chains import Chain
+from app.portfolio import Portfolio
+from app.utils import clean_text
 
 
 def create_streamlit_app(llm, portfolio, clean_text):
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+import streamlit as st
+from langchain_community.document_loaders import WebBaseLoader
