022b5d6de2b6ea6c703152cff374929cf6d7d372
abhijat-0 <abhijatknight003@gmail.com>
2025-04-19 11:46:43 +0530

    update

--- portfolio.py
+++ portfolio.py
@@ -4,7 +4,7 @@ import uuid
 
 
 class Portfolio:
-    def __init__(self, file_path="app/resource/my_portfolio.csv"):
+    def __init__(self, file_path="resource\my_portfolio.csv"):
         self.file_path = file_path
         self.data = pd.read_csv(file_path)
         self.chroma_client = chromadb.PersistentClient('vectorstore')
